6289b20e0dce47887d87c0f41ccfa3fdbf648a18
Luis Hebendanz <luis.nixos@gmail.com>
2020-01-24 23:13:04 +0100

    Added latex & typescript language server support

--- kakoune.nix
+++ kakoune.nix
@@ -6,9 +6,38 @@ unstable = import <nixos-unstable> {};
 
 kak_lsp_conf = pkgs.writeText "kak-lsp.toml" ''
 [language.typescript]
-filetypes = ["typescript"]
+filetypes = ["typescript", "javascript"]
 roots = ["package.json", "tsconfig.json"]
 command = "javascript-typescript-stdio"
+
+[language.python]
+filetypes = ["python"]
+roots = ["requirements.txt", "setup.py", ".git", ".hg"]
+command = "pyls"
+offset_encoding = "utf-8"
+
+[language.c_cpp]
+filetypes = ["c", "cpp"]
+roots = ["compile_commands.json", ".cquery"]
+command = "cquery"
+args = ["--init={\"cacheDirectory\":\"/tmp/cquery\",\"highlight\":{\"enabled\":true}}"]
+
+[language.bash]
+filetypes = ["sh"]
+roots = [".git", ".hg"]
+command = "bash-language-server"
+args = ["start"]
+
+[language.latex]
+filetypes = ["latex"]
+roots = [".git"]
+command = "texlab"
+
+[language.go]
+filetypes = ["go"]
+roots = ["Gopkg.toml", "go.mod", ".git", ".hg"]
+command = "gopls"
+offset_encoding = "utf-8"
 '';
 
 kak_config = pkgs.writeText "kakrc" ''
@@ -26,15 +55,17 @@ environment.systemPackages = with pkgs; [
 	providers=["mccabe" "rope" "yapf" "pyflakes"];
     })
     nodePackages.javascript-typescript-langserver
+    nodePackages.bash-language-server
     python37Packages.pyls-black
     python37Packages.pyls-isort
     python37Packages.pyls-mypy
     rls
+    unstable.texlab # Latex language server
     unstable.kak-lsp
     (unstable.kakoune.override {
     configure = {
         plugins = with unstable.pkgs.kakounePlugins; [ 
-          kak-powerline
+          kak-powerline # Neat colored status bar
           kak-buffers # Easy buffer management
           kak-fzf # Intergrated fzf
         ];
